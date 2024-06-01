d1b5c816590bb498894111d77c066faa73fe45a0
Ryuko <ryuko@peek1e.eu>
2024-06-01 16:30:18 +0200

    fix: ssh key creation

--- tofu_infra/ssh-key.tf
+++ tofu_infra/ssh-key.tf
@@ -3,11 +3,13 @@ resource "tls_private_key" "ssh-key" {
 }
 
 resource "local_file" "public_key" {
-  filename = "~/.ssh/tofu-ssh.pub"
+  filename = "${path.cwd}/local/tofu-ssh.pub"
+  file_permission = "0600"
   content  = tls_private_key.ssh-key.public_key_openssh
 }
 
 resource "local_file" "private_key" {
-  filename = "~/.ssh/tofu-ssh"
+  filename = "${path.cwd}/local/tofu-ssh"
+  file_permission = "0600"
   content  = tls_private_key.ssh-key.private_key_openssh
 }
